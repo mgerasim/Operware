53adf955942a4bf612f2f8d677b89c87c44921bc
Mikhail Gerasimov <mgerasim@inbox.ru>
2020-02-22 12:35:06 +1000

    gitignore

--- Server/dist/Server/start.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const exampleServer = new server_1.default();
-exampleServer.start(3000);
-//# sourceMappingURL=start.js.map
