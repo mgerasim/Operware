6bc8c8032f5772ab9947989b19a87e0b6858b0b3
Mikhail Gerasimov <mgerasim@inbox.ru>
2020-04-13 21:05:39 +1000

    build

--- WebFronted/Operware/deploy.ast12.sh
+++ WebFronted/Operware/deploy.ast12.sh
@@ -2,6 +2,6 @@ git add .
 git commit -m build
 git pull origin master
 git push origin master
-ng build --optimization
+ng build 
 scp dist/Operware/* rails@ast12.site:~/projects/Operware/Server/public
 
