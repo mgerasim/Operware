481d3f9408556a6d82de8c084c6b1632f57732a2
Mikhail Gerasimov <mgerasim@inbox.ru>
2020-04-17 01:15:17 +0300

    deploy

---
+++ Server/deploy.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -m deploy
+git pull origin master
+git push origin master
+cd ~/apps/Integrity
+git push origin master
--- Server/src/start.ts
+++ Server/src/start.ts
@@ -3,6 +3,6 @@ import ExampleServer from './server';
 
 
 const exampleServer = new ExampleServer();
-const port = process.env.NODE_ENV === 'production' ? 8080 : parseInt(process.env.PORT, 3000) || 3000;
+const port = process.env.NODE_ENV === 'production' ? 9080 : parseInt(process.env.PORT, 3000) || 3000;
 
 exampleServer.start(port);
