5bdb16b5f83a542064fbc12d7bf13d9ff13c8264
Mikhail Gerasimov <mgerasim@inbox.ru>
2020-04-17 01:28:57 +0300

    deploy

--- Server/install.sh
+++ Server/install.sh
@@ -3,7 +3,6 @@ git commit -m deploy
 git pull origin master
 git push origin master
 sudo systemctl stop Integrity.service
-sequelize db:migrate --env production
 sudo cp Integrity.service /etc/systemd/system/
 sudo service systemd-resolved restart
 sudo systemctl enable Integrity.service
