944036e0a92cf7e99ef2ecfedbe7d325f15881d2
Mikhail Gerasimov <mgerasim@inbox.ru>
2020-04-20 20:16:47 +1000

    build

--- WebFronted/Operware/deploy.sh
+++ WebFronted/Operware/deploy.sh
@@ -2,6 +2,5 @@ git add .
 git commit -m build
 git pull origin master
 git push origin master
-ng build --optimization
-scp dist/Integrity/* misha@dev84:~/projects/Integrity/Server/public
-scp dist/Integrity/assets/* misha@dev84:~/projects/Integrity/Server/public/assets
+ng build
+scp dist/Operware/* misha@dev84:~/projects/Integrity/Server/public
